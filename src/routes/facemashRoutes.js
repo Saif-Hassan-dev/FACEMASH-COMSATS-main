a8b1cdd43d9f81c5d6c5d1e657abb27e9a1ed195
saif ul hassan <SP25-BCS-040@cuiatd.edu.pk>
2026-01-28 12:44:08 +0500

    Export facemashRoutes module
    Added module exports for facemashRoutes.

--- src/routes/facemashRoutes.js
+++ src/routes/facemashRoutes.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 
 const controller = new FacemashController();
-
+//module exports
 module.exports = (app) => {
     app.get('/api/users', controller.getAllUsers);
     app.post('/api/upload', upload.single('image'), controller.uploadUser);

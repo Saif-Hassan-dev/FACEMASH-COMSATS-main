e962c22585e6bf2ed6c1863c642df473b90cfd4a
saif ul hassan <SP25-BCS-040@cuiatd.edu.pk>
2026-01-30 11:13:33 +0500

    Add comment in helpers.js for formatUserData
    Add a comment for formatting user data function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,7 @@
 export function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+//for formatiign users data
 
 export function formatUserData(user) {
     return {

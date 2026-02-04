361a76ec35b3d53c5308a3cf93dbb53030a7a1ea
saif ul hassan <SP25-BCS-040@cuiatd.edu.pk>
2026-02-04 11:12:54 +0500

    Fix typo in multer storage comment

--- src/routes/facemashRoutes.js
+++ src/routes/facemashRoutes.js
@@ -1,7 +1,7 @@
 const { FacemashController, getLeaderboard } = require('../controllers/facemashController');
 const multer = require('multer');
 
-// Use memory storage for multer (for GCS upload)
+// Use memory storagee for multer (for GCS upload)
 const upload = multer({ storage: multer.memoryStorage() });
 
 const controllers = new FacemashController();
